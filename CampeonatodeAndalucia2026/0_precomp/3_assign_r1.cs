3f805ece6303d86cfad6f62cf357d8c62307c66b
aasalobrena <alvaroaasgm@gmail.com>
2026-02-28 20:59:43 +0100

    take it easy

--- CampeonatodeAndalucia2026/0_precomp/3_assign_r1.cs
+++ CampeonatodeAndalucia2026/0_precomp/3_assign_r1.cs
@@ -9,11 +9,17 @@ Map([_666-r1, _777-r1, _333bf-r1, _clock-r1, _minx-r1, _skewb-r1],
     AssignFixedGroups(Arg<Round>())
    )
 
-Map([_777-r1, _666-r1, _444-r1, _pyram-r1, _sq1-r1, _skewb-r1,
-     _333-r1, _222-r1, _333oh-r1, _333bf-r1, _minx-r1],
-    AssignVolunteers(Arg<Round>(), 2026-03-07, 16, 3, ScramblersPerRound(Arg<Round>()))
-   )
+AssignVolunteers(_777-r1, 2026-03-07, 13, 3, 2)
+AssignVolunteers(_666-r1, 2026-03-07, 14, 3, 2)
+AssignVolunteers(_444-r1, 2026-03-07, 16, 3, 3)
+AssignVolunteers(_pyram-r1, 2026-03-07, 16, 3, 3)
+AssignVolunteers(_sq1-r1, 2026-03-07, 16, 3, 3)
+AssignVolunteers(_skewb-r1, 2026-03-07, 16, 3, 3)
+AssignVolunteers(_333-r1, 2026-03-07, 16, 3, 3)
+AssignVolunteers(_222-r1, 2026-03-07, 16, 3, 3)
+AssignVolunteers(_333oh-r1, 2026-03-07, 16, 3, 3)
+AssignVolunteers(_333bf-r1, 2026-03-07, 13, 3, 2)
+AssignVolunteers(_minx-r1, 2026-03-07, 14, 3, 2)
 
-Map([_clock-r1, _555-r1],
-    AssignVolunteers(Arg<Round>(), 2026-03-08, 16, 3, ScramblersPerRound(Arg<Round>()))
-   )
+AssignVolunteers(_clock-r1, 2026-03-08, 14, 3, 3)
+AssignVolunteers(_555-r1, 2026-03-08, 16, 3, 4)
--- CampeonatodeAndalucia2026/lib/_assign.cs
+++ CampeonatodeAndalucia2026/lib/_assign.cs
@@ -6,40 +6,6 @@
 
 # Args:
 # 1: Round
-Define("ScramblersPerRound",
-       Switch({1, Round},
-              [Tuple(_333-r1, 4),
-               Tuple(_333-r2, 3),
-               Tuple(_333-r3, 3),
-               Tuple(_222-r1, 4),
-               Tuple(_222-r2, 3),
-               Tuple(_444-r1, 4),
-               Tuple(_444-r2, 3),
-               Tuple(_555-r1, 4),
-               Tuple(_555-r2, 3),
-               Tuple(_666-r1, 3),
-               Tuple(_777-r1, 3),
-               Tuple(_333bf-r1, 3),
-               Tuple(_333bf-r2, 3),
-               Tuple(_333oh-r1, 3),
-               Tuple(_333oh-r2, 3),
-               Tuple(_clock-r1, 3),
-               Tuple(_clock-r2, 3),
-               Tuple(_minx-r1, 3),
-               Tuple(_minx-r2, 3),
-               Tuple(_pyram-r1, 4),
-               Tuple(_pyram-r2, 3),
-               Tuple(_skewb-r1, 3),
-               Tuple(_skewb-r2, 3),
-               Tuple(_sq1-r1, 3),
-               Tuple(_sq1-r2, 3)
-              ],
-              0
-             )
-      )
-
-# Args:
-# 1: Round
 Define("AssignWaitingGroups",
        AssignGroups({1, Round},
                     NormalRoundAssignmentSets({1, Round}), [], []
