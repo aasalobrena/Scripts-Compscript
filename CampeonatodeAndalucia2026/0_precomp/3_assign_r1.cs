6883798ba77e2d58308428d67bbe8ef154ea8eea
aasalobrena <alvaroaasgm@gmail.com>
2026-02-28 01:51:36 +0100

    update with real data

--- CampeonatodeAndalucia2026/0_precomp/3_assign_r1.cs
+++ CampeonatodeAndalucia2026/0_precomp/3_assign_r1.cs
@@ -1,19 +1,19 @@
 #include "../lib/_constants.cs"
 #include "../lib/_assign.cs"
 
-Map([_333-r1, _222-r1, _444-r1, _555-r1, _333oh-r1, _pyram-r1, _skewb-r1, _sq1-r1],
+Map([_333-r1, _222-r1, _444-r1, _555-r1, _333oh-r1, _pyram-r1, _sq1-r1],
     AssignWaitingGroups(Arg<Round>())
    )
 
-Map([_666-r1, _777-r1, _333bf-r1, _clock-r1, _minx-r1],
+Map([_666-r1, _777-r1, _333bf-r1, _clock-r1, _minx-r1, _skewb-r1],
     AssignFixedGroups(Arg<Round>())
    )
 
 Map([_777-r1, _666-r1, _444-r1, _pyram-r1, _sq1-r1, _skewb-r1,
      _333-r1, _222-r1, _333oh-r1, _333bf-r1, _minx-r1],
-    AssignVolunteers(Arg<Round>(), 2026-03-07, 16, 3, 4)
+    AssignVolunteers(Arg<Round>(), 2026-03-07, 16, 3, ScramblersPerRound(Arg<Round>()))
    )
 
 Map([_clock-r1, _555-r1],
-    AssignVolunteers(Arg<Round>(), 2026-03-08, 16, 3, 4)
+    AssignVolunteers(Arg<Round>(), 2026-03-08, 16, 3, ScramblersPerRound(Arg<Round>()))
    )
